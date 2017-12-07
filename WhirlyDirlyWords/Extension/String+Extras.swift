e636d68b37be93203820fe8c99e6494de592a9ed
Justin Veach <justinwveach@gmail.com>
2017-12-06 22:09:53 -0600

    Extending String

---
+++ WhirlyDirlyWords/Extension/String+Extras.swift
@@ -0,0 +1,24 @@
+
+//
+//  File.swift
+//  WhirlyDirlyWords
+//
+//  Created by Justin Veach on 12/6/17.
+//  Copyright © 2017 justinveach. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    func length() -> Length {
+        if self.count < 5 {
+            return .short
+        } else if self.count >= 5 && self.count <= 7 {
+            return .medium
+        } else {
+            return .long
+        }
+    }
+    
+}
