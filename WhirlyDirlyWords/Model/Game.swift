e128adffcaad218afe00c0563defef447834534f
Justin Veach <justinwveach@gmail.com>
2018-02-02 14:11:38 -0600

    WIP game logic

--- WhirlyDirlyWords/Model/Game.swift
+++ WhirlyDirlyWords/Model/Game.swift
@@ -15,9 +15,19 @@ struct Game {
     var currentRound: Int = 1
     var score: Int = 0
     
-    var gameFinished = false
+    var puzzleGenerator = CrosswordGenerator(words: Words.sharedInstance)
+    let roundsPerCheckpoint = 5
+    var currentPuzzle: CrosswordPuzzle?
     
     var delegate: GameDelegate!
+
+    var puzzleSize: Int {
+        get {
+            // todo: logic to calculate size based on round
+            return 8
+        }
+    }
+    var gameFinished = false
     
     init(delegate: GameDelegate) {
         self.delegate = delegate
@@ -28,9 +38,30 @@ struct Game {
         self.startingRound = startingRound
     }
     
+    mutating func startNextRound() -> (CrosswordPuzzle, [Character]) {
+        currentPuzzle = puzzleGenerator.createPuzzle(wordStructure: [.short, .medium, .short, .short], size: puzzleSize)
+        currentPuzzle?.printResult()
+        
+        let userPuzzle = CrosswordPuzzle(size: puzzleSize)
+        for t in (currentPuzzle?.tiles)! {
+            let tile = Tile(letter: " ", column: t.column, row: t.row)
+            userPuzzle.add(tile)
+        }
+        
+        //puzzle.letters shuffles the letters for us, so let's keep that order once we shuffle
+        let letters = currentPuzzle?.letters ?? []
+        
+        return (userPuzzle, letters)
+        
+    }
+    
     func completedRound(numberOfLetters: Int, numberOfWords: Int) {
         // caculate score
         //delegate.updated(score: score)
+    }
+    
+    fileprivate func getPuzzleWords() -> [Length] {
+        return [.short]
     }
     
 }
